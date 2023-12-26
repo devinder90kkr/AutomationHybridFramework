844cf512791c02cae7413e1979d3054c21d73389
Chicmic <chicmic@192-180-005-027.res.spectrum.com>
2023-12-26 13:34:10 +0530

    update config file

---
+++ README.md
@@ -0,0 +1,9 @@
+*Project overview file *
+
+
+Command run for jenkins on mac
+
+* Install the latest LTS version: >> brew install jenkins-lts
+* Start the Jenkins service:  >> brew services start jenkins-lts
+* Restart the Jenkins service: >> brew services restart jenkins-lts
+* Update the Jenkins version: >> brew upgrade jenkins-lts
--- src/main/java/cuesz/utils/Configuration.java
+++ src/main/java/cuesz/utils/Configuration.java
@@ -2,18 +2,18 @@ package cuesz.utils;
 
 public class Configuration {
     public static final String BASE_URL = "https://pre-staging.app.cuesz.com";
-    //Window system file location
-     public static final String CHROME_DRIVER_PATH = "C:\\Users\\ASUS\\OneDrive\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe";
+//    //Window system file location
+//     public static final String CHROME_DRIVER_PATH = "C:\\Users\\ASUS\\OneDrive\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe";
      
-    // mac system file location
-    // public static final String CHROME_DRIVER_PATH =	"/Users/chicmic/Documents/chromedriver-mac-x64/chromedriver";	
+//     mac system file location
+     public static final String CHROME_DRIVER_PATH =	"/Users/chicmic/Documents/chromedriver-mac-x64/chromedriver";	
     
-//     // mac edge driver path
-//    public static final String EDGE_DRIVER_PATH =	"/Users/chicmic/Documents/Edgedriver-mac-x64/msedgedriver";
+     // mac edge driver path
+    public static final String EDGE_DRIVER_PATH =	"/Users/chicmic/Documents/Edgedriver-mac-x64/msedgedriver";
  
- // Windows edge driver path
-     public static final String EDGE_DRIVER_PATH =	"C:\\Users\\ASUS\\OneDrive\\Documents\\edgedriver_win64\\msedgedriver.exe\\";
-    
+// // Windows edge driver path
+//     public static final String EDGE_DRIVER_PATH =	"C:\\Users\\ASUS\\OneDrive\\Documents\\edgedriver_win64\\msedgedriver.exe\\";
+//    
     
     public static final String SAFARI_DRIVER_PATH =	"/usr/bin/safaridriver"; 
 
