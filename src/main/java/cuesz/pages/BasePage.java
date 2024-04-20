d129e43f2f74d327728f4ddf2de2f0dbb763f3de
devinder90kkr <devinder90kkr@gmail.com>
2024-04-20 19:03:17 +0530

    updated comment in BasePage class

--- src/main/java/cuesz/pages/BasePage.java
+++ src/main/java/cuesz/pages/BasePage.java
@@ -1,20 +1,21 @@
+// package and import statement 
 package cuesz.pages;
-
 import org.openqa.selenium.WebDriver;
 
-public class BasePage {
-	
-	protected WebDriver driver;
+public class BasePage {						//Class declaration
+		
+	protected WebDriver driver;				//Class Variables : declares a protected instance variable driver of type WebDriver
 
-	public BasePage(WebDriver driver) {
-    this.driver = driver;
+// Define Constructor 
+	public BasePage(WebDriver driver) {     // is the constructor of the BasePage class.
+    this.driver = driver; 					// Inside the constructor, this.driver = driver; assigns the driver parameter passed to the BasePage class's driver variable.
 }
 
 // Common methods that can be used across different page classes
-public void navigateTo(String url) {
-    driver.navigate().to(url);
+public void navigateTo(String url) { 			// public void navigateTo(String url) is a method defined within the BasePage class.
+												// This method takes a String parameter url
+    driver.navigate().to(url);					// Inside the method, driver.navigate().to(url); uses the driver instance to navigate the browser to the specified URL (url).
     
 }
 
-// Add more common methods here...
 }
